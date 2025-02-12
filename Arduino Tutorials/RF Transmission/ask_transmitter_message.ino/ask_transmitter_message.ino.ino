3889674854e8425a703e9f289b5482e3be7c38a2
Bryce K <68964590+bakeever@users.noreply.github.com>
2025-02-11 20:47:26 -0700

    Update

---
+++ Arduino Tutorials/RF Transmission/ask_transmitter_message.ino/ask_transmitter_message.ino.ino
@@ -0,0 +1,24 @@
+// Include RadioHead Amplitude Shift Keying Library
+#include <RH_ASK.h>
+// Include dependant SPI Library 
+#include <SPI.h> 
+ 
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+ 
+void setup()
+{
+    Serial.begin(9600);
+    Serial.print("Message Sending");
+    // Initialize ASK Object
+    rf_driver.init();
+}
+ 
+void loop()
+{
+    const char *msg = "Hello World";
+    rf_driver.send((uint8_t *)msg, strlen(msg));
+    rf_driver.waitPacketSent();
+    delay(1000);
+    Serial.println(msg);
+}
